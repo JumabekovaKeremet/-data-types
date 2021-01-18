b81ac65809457361198c6582178870da64f0bf17
ДжумабековаКеремет <jumabekovak15@gmail.com>
2021-01-18 15:50:05 +0600

    changed scripts

---
+++ scripts.js
@@ -0,0 +1,92 @@
+let string1= "Hello world";
+let string2= 'World';
+
+
+
+let boolean1= true;
+let boolean1= false;
+
+
+let undefind1= undefind;
+let undefind2;
+
+let null1= null;
+
+let person={
+    firstName: "Keremet" ;
+    lastName: "Jumabekova" ;
+    getFullName: () => person.firstName + " " + person.lastName ;
+
+};
+console.log(person);
+
+let math1 = 1 + 1;
+let math2 = 1 - 1;
+let math3 = 1 * 1;
+let math4 = 1 / 1;
+let math5 = 5 ** 2;
+let math6 = 6 ** 5;
+let math7 = 9 ** (1/2);
+let math8 = 10 % 4;
+let math9 = 100 % 12;
+
+let example1 = 1;
+//increment
+example1 = example1 + 1;
+example += 1;
+example1++;
+//decrement
+ example1 += 5;
+example1 += 15;
+example1 = example1 -1;
+example1 -= 1;
+example1--;
+
+example1 = example1 / 2;
+example1 /= 2;
+
+example1a = example1 * 2;
+example1 *= 2;
+
+let example2 = 1 - -1;
+let example3 = +1;
+
+let example4 = 200 / 100 * 20;
+let example5 = 300 / 100 * 85;
+
+let e = 12;
+let example6 = 100 + 5 * 3 - -e/2  + 12 ** (1/2);
+
+let a = 10;
+let b = 5;
+let c;
+let d = 5 +(c = a + b);
+
+let primer1 = 1 + "1";//11
+let primer2 = "1" + 1; //11
+let primer3 = 1 + 1 + "1"; //21
+let primer4 = "1" + 1 + 1; //111
+let primer5 = 5 / 5 + "4";
+let primer6 = 12 - "Hello";//NaN
+let primer7 = 12 + 6 + 1 / "B";//NaN
+ let primer8 = "1";
+ console.log(+primer8 + "1");//добавиться
+console.log(primer1);
+
+let primer9 = Namber ("1234");
+
+let primer10 = Math.randome() * 100; //от 0 до 100
+100 + Math.randome() * 400;
+Math.round(Math.randome() * 100 );
+Math.ceil(3.5);
+Math.floor(3.5);
+let example = 3.14123123124125;
+example.toFixed(5);
+parseInt("15 bananas");
+parseFloat("15.5 bananas");
+
+let aa = prompt("How many dollars do you have?");
+let bb = prompt("What is dollar exchange rate? ");
+let cc = parseFloat(a) * parseFloat(bb);
+
+alert("You have " + cc + "soms!");
